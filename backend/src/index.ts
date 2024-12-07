c3347280d0ca6968e73fac48f624fcc09da35c7e
Kacper Wanat <kacperryszardwanat@gmail.com>
2024-12-07 16:47:42 +0100

    initializing project

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/api", (req: Request, res: Response) =>
+{
+    res.send("Hello, world!");
+});
+
+app.listen(PORT, () =>
+{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
